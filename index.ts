d81d67cf8a8465b29fa06fb9a8aef260309ed94c
kumastry <chma19063@g.nihon-u.ac.jp>
2024-05-12 08:18:06 +0900

    スクリプトやスタイルを追加した

--- index.ts
+++ index.ts
@@ -1,16 +1,25 @@
 import express from "express";
+import path from "path";
 
 const app = express();
-const port = 3002;
+const port = 3000;
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
 
-app.get('/', (request, response) => {
+app.use((req, res, next) => {
+  console.log(req.method);
+  console.log(req.path);
+  console.log(req.query.name)
+  next();
+})
+
+app.get('/', (req, res) => {
   const html = '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /> <meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Document</title></head><body><h1>KUMASTRY</h1></body><script>console.log("kumastry");</script></html>'
-  response.send(html);
+  res.send(html);
 });
 
 app.get('/index', (req, res) => {
-  res.render('index', {name: 'kimastry'});
+  res.status(200).render('index', {name: 'kimastry'});
 });
 
 app.listen(port, () => {
---
+++ public/script/button_event.js
@@ -0,0 +1,4 @@
+console.log("kumastry");
+document.querySelector("#add_task_button").addEventListener("click", () => {
+    alert("task added!!!!!!!");
+});
