ae45057c5a4ef70a58b28a68a8933fe3f044d42e
kumastry <chma19063@g.nihon-u.ac.jp>
2024-05-11 18:00:20 +0900

    ejsの追加

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 3002;
+app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-  const html = '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /> <meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Document</title></head><body><h1>KUMASTRY</h1></body></html>'
+  const html = '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /> <meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Document</title></head><body><h1>KUMASTRY</h1></body><script>console.log("kumastry");</script></html>'
   response.send(html);
+});
+
+app.get('/index', (req, res) => {
+  res.render('index', {name: 'kimastry'});
 });
 
 app.listen(port, () => {
