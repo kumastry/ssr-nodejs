463e70be774840095d4cb77c9c69155c78bca5ad
kumastry <chma19063@g.nihon-u.ac.jp>
2024-05-12 23:18:29 +0900

    indexページへのリンクを作成した

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-  const html = '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /> <meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Document</title></head><body><h1>KUMASTRY</h1></body><script>console.log("kumastry");</script></html>'
+  const html = '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /> <meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Document</title></head><body><h1>KUMASTRY</h1><a href = "/index">index link</a></body><script>console.log("kumastry");</script></html>'
   res.send(html);
 });
 
